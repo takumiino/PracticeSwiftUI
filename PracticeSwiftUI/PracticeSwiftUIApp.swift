d13c8f08d69b9151bc48d75b27d4079b342be697
takumiino <hal.bandotakumi@gmail.com>
2024-11-28 03:55:43 +0900

    Initial Commit

---
+++ PracticeSwiftUI/PracticeSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeSwiftUIApp.swift
+//  PracticeSwiftUI
+//
+//  Created by 拓実 on 2024/11/28.
+//
+
+import SwiftUI
+
+@main
+struct PracticeSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
